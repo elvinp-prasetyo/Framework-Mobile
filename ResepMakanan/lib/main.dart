a04cc32af7971bfbc72983286a6b701fbc3fc7f8
elvin prasetyo <elvinpras8883@gmail.com>
2022-08-22 15:39:19 +0700

    Project

---
+++ ResepMakanan/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/data/homescreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // membuat tampilan home dengan judul resep makanan.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Resep Makanan',
+      theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.purple),
+      home: const home(),
+    );
+  }
+}
